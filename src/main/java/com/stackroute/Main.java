0092cc05816057633179674a8bd97b60f6adff23
prasanth-priyan <prasanth.priyan@gmail.com>
2019-09-24 13:00:49 +0530

    First Branch Commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,13 +1,7 @@
 package com.stackroute;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 /**
  * Hello world!
@@ -17,18 +11,13 @@ public class Main
 {
     public static void main( String[] args )
     {
-        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie baahubali = (Movie) factory.getBean("movie");
-        baahubali.starring();
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Movie baahubali1 = context.getBean("movie1", Movie.class);
+        baahubali1.starring();
 
-        BeanDefinitionRegistry registry = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        BeanDefinitionReader reader = new XmlBeanDefinitionReader(registry);
-        BeanFactory beans = (BeanFactory) reader.getRegistry();
-        Movie baahubali2 = (Movie) beans.getBean("movie");
+        Movie baahubali2 = context.getBean("movie2", Movie.class);
         baahubali2.starring();
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie baahubali3 = context.getBean("movie", Movie.class);
-        baahubali3.starring();
+        System.out.println(baahubali1==baahubali2);
     }
 }
